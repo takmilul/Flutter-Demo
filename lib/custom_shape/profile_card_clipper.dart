08e2a6173a291f34fe497acb170d4937b0a125ff
Takmilul Ehsan <tonmoy4222@gmail.com>
2019-05-30 16:39:22 +0600

    profile view commit

--- lib/custom_shape/profile_card_clipper.dart
+++ lib/custom_shape/profile_card_clipper.dart
@@ -12,9 +12,9 @@ class ProfileCardClipper extends CustomClipper<Path>{
     var firstEndPoint = Offset(size.width/2, 40);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
     
-    /*var secondControlPoint = Offset(size.width * 0.65, 60);
-    var secondEndPoint = Offset(size.width * 0.60, 0.0);
-    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);*/
+    var secondControlPoint = Offset(size.width/2+40, 40.0);
+    var secondEndPoint = Offset(size.width/2+40, 0.0);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
     
     path.lineTo(size.width * 0.5, 0.0);
     path.lineTo(size.width, 0.0);
